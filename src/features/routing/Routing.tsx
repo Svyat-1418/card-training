dda064656278a88ef2f759173f88ca7a14590e12
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-08-22 18:49:23 +0300

    <Layout /> component implemented using <Outlet />

--- src/features/routing/Routing.tsx
+++ src/features/routing/Routing.tsx
@@ -5,17 +5,20 @@ import { SignUp } from '../signUp/SignUp'
 import { SignIn } from '../signIn/SignIn'
 import { PersonalInformation } from '../personalInformation/PersonalInformation'
 import { ForgotPassword } from '../recoveryPassword/ForgotPassword'
+import { Layout } from './Layout'
 
 export const Routing = () => {
   return (
     <Routes>
-      <Route path={`${Path.SingUp}`} element={<SignUp />} />
-      <Route path={Path.SingIn} element={<SignIn />} />
-      <Route path={Path.PersonalInformation} element={<PersonalInformation />} />
-      <Route path={Path.ForgotPassword} element={<ForgotPassword />} />
-      <Route path={Path.CheckEmail} element={<></>} />
-      <Route path={Path.CreateNewPassword} element={<></>} />
-      <Route path={Path.Page404} element={<></>} />
+      <Route path={'/'} element={<Layout />}>
+        <Route path={`${Path.SingUp}`} element={<SignUp />} />
+        <Route path={Path.SingIn} element={<SignIn />} />
+        <Route path={Path.PersonalInformation} element={<PersonalInformation />} />
+        <Route path={Path.ForgotPassword} element={<ForgotPassword />} />
+        <Route path={Path.CheckEmail} element={<></>} />
+        <Route path={Path.CreateNewPassword} element={<></>} />
+        <Route path={Path.Page404} element={<></>} />
+      </Route>
     </Routes>
   )
 }
