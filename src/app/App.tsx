f7f82482f539768dbd513b024a65b6fa9ab0cc53
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-08-22 18:32:25 +0300

    <Routing/> component implemented

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import { Routing } from '../features/routing/Routing'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Routing />
+    </div>
+  )
 }
 
 export default App
---
+++ src/features/routing/Routing.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Path } from '../../common/enums/Path'
+import { SignUp } from '../signUp/SignUp'
+import { SignIn } from '../signIn/SignIn'
+import { PersonalInformation } from '../personalInformation/PersonalInformation'
+import { ForgotPassword } from '../recoveryPassword/ForgotPassword'
+
+export const Routing = () => {
+  return (
+    <Routes>
+      <Route path={`${Path.SingUp}`} element={<SignUp />} />
+      <Route path={Path.SingIn} element={<SignIn />} />
+      <Route path={Path.PersonalInformation} element={<PersonalInformation />} />
+      <Route path={Path.ForgotPassword} element={<ForgotPassword />} />
+      <Route path={Path.CheckEmail} element={<></>} />
+      <Route path={Path.CreateNewPassword} element={<></>} />
+      <Route path={Path.Page404} element={<></>} />
+    </Routes>
+  )
+}
