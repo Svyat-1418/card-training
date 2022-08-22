66e0e9a849e928d1fcfe757b65ce3259a30310d4
Dmitriy Yuganuk <dmitriyyuganuk@gmail.com>
2022-08-22 22:06:00 +0300

    ForgotPassword component added to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import { ForgotPassword } from '../features/recoveryPassword/ForgotPassword'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <ForgotPassword />
+    </div>
+  )
 }
 
 export default App
