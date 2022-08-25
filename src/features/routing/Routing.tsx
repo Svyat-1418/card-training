3076fc14a4d5183ccc419c7711cb1a0622f28d9d
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-08-26 00:07:19 +0300

    component <Routing /> was corrected

--- src/features/routing/Routing.tsx
+++ src/features/routing/Routing.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Path } from '../../common/enums/Path'
 import { SignUp } from '../signUp/SignUp'
 import { SignIn } from '../signIn/SignIn'
@@ -8,18 +8,24 @@ import { ForgotPassword } from '../recoveryPassword/ForgotPasswordPage/ForgotPas
 import { Layout } from './Layout'
 import { CreateNewPassword } from '../recoveryPassword/CreateNewPasswordPage/CreateNewPassword'
 import { CheckEmail } from '../recoveryPassword/CheckEmailPage/CheckEmail'
+import Typography from '@mui/material/Typography'
 
 export const Routing = () => {
   return (
     <Routes>
       <Route path={'/'} element={<Layout />}>
+        <Route index element={<PersonalInformation />} />
         <Route path={`${Path.SingUp}`} element={<SignUp />} />
         <Route path={Path.SingIn} element={<SignIn />} />
         <Route path={Path.PersonalInformation} element={<PersonalInformation />} />
         <Route path={Path.ForgotPassword} element={<ForgotPassword />} />
         <Route path={Path.CheckEmail} element={<CheckEmail />} />
         <Route path={Path.CreateNewPassword} element={<CreateNewPassword />} />
-        <Route path={Path.Page404} element={<></>} />
+        <Route
+          path={Path.Page404}
+          element={<Typography variant={'h3'}>PAGE NOT FOUND :-(</Typography>}
+        />
+        <Route path={'*'} element={<Navigate to={Path.Page404} />} />
       </Route>
     </Routes>
   )
