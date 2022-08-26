09c31cc58d50e7d5ef599db181a13d0e2ef2f344
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-08-26 10:57:26 +0300

    MeResponseType added

--- src/features/auth/authApi.ts
+++ src/features/auth/authApi.ts
@@ -2,7 +2,7 @@ import { instance } from '../../common/config/apiConfig'
 
 export const authAPI = {
   me() {
-    return instance.post('auth/me')
+    return instance.post<MeResponseType>('auth/me')
   },
   logout() {
     return instance.delete('auth/me')
@@ -15,4 +15,24 @@ export const authAPI = {
 export type UpdateMeModelType = {
   name?: string
   avatar?: string
+}
+
+export type MeResponseType = {
+  _id: string
+  email: string
+  rememberMe: boolean
+  isAdmin: boolean
+  name: string
+
+  avatar?: string
+
+  verified: boolean
+  publicCardPacksCount: number
+  created: string
+  updated: string
+  __v: number
+  token: string
+  tokenDeathTime: number
+
+  error?: string
 }
