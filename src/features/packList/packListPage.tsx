f0e0077b2b888eba6eb9e0c91539f417d289eebe
Dmitriy Yuganuk <dmitriyyuganuk@gmail.com>
2022-08-27 13:32:54 +0300

    packagePage column names added

--- src/features/packList/packListPage.tsx
+++ src/features/packList/packListPage.tsx
@@ -25,11 +25,11 @@ export const PackListPage = () => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Cards</TableCell>
+            <TableCell align="right">Last updated</TableCell>
+            <TableCell align="right">Created by</TableCell>
+            <TableCell align="right">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
