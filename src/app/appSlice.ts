b77c710e83e3dacfc8d70255d23ee2565fe404a4
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-08-29 20:55:57 +0300

    initialize app fixed

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,18 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { authAPI, MeResponseType } from '../features/auth/authApi'
 import { handleNetworkError } from '../common/utils/errorUtil'
-
-export const initializeApp = createAsyncThunk(
-  'app/initializeApp',
-  async (payload, { fulfillWithValue, dispatch }) => {
-    try {
-      const res = await authAPI.me()
-      return fulfillWithValue<MeResponseType>(res.data)
-    } catch (err: any) {
-      handleNetworkError(err, dispatch)
-    }
-  }
-)
+import { setIsLoggedInAC } from '../features/signIn/signIn-slice'
 
 const initialState = {
   status: 'idle' as RequestStatusType,
@@ -20,27 +9,47 @@ const initialState = {
   isInitialized: false,
   userData: {} as MeResponseType,
 }
-
 const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+    setStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
       state.status = action.payload.status
     },
-    setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
+    setError: (state, action: PayloadAction<{ error: string | null }>) => {
       state.error = action.payload.error
+    },
+    setIsInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
+      state.isInitialized = action.payload.isInitialized
     },
   },
   extraReducers: (builder) => {
     builder.addCase(initializeApp.fulfilled, (state, action) => {
-      if (action.payload) state.userData = action.payload.payload
+      if (action.payload) {
+        state.userData = action.payload.userData
+        state.isInitialized = true
+      }
       state.isInitialized = true
     })
   },
 })
 
-export const { setAppStatus, setAppError } = appSlice.actions
+export const initializeApp = createAsyncThunk(
+  'app/initializeApp',
+  async (_, { dispatch }) => {
+    try {
+      const res = await authAPI.me()
+      dispatch(setIsLoggedInAC({ value: true }))
+      return { userData: res.data }
+    } catch (err: any) {
+      handleNetworkError(err, dispatch)
+    } finally {
+      dispatch(setIsInitialized)
+    }
+  }
+)
+
+export const { setStatus, setError, setIsInitialized } = appSlice.actions
 export const appReducer = appSlice.reducer
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
