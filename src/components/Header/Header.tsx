6bddfe9d7503e2c8f131acba2a38dfbc89e3b17d
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-08-31 17:29:29 +0300

    Linear Progress display is implemented when the status is 'loading'

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,25 +2,32 @@ import React from 'react'
 
 import AppBar from '@mui/material/AppBar'
 import Container from '@mui/material/Container'
+import LinearProgress from '@mui/material/LinearProgress'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 
 import style from './Header.module.css'
 import { UserInfo } from './UserInfo'
 import { AccountSettings } from './AccountSettings'
+import { useAppSelector } from '../../common/hooks'
 
 export const Header = () => {
+  const status = useAppSelector((state) => state.app.status)
+
   return (
-    <AppBar>
-      <Container>
-        <Toolbar className={style.toolbar}>
-          <UserInfo />
+    <>
+      <AppBar>
+        <Container>
+          <Toolbar className={style.toolbar}>
+            <UserInfo />
 
-          <Typography variant={'h4'}>Card Training</Typography>
+            <Typography variant={'h4'}>Card Training</Typography>
 
-          <AccountSettings />
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <AccountSettings />
+          </Toolbar>
+        </Container>
+        {status === 'loading' && <LinearProgress color={'success'} />}
+      </AppBar>
+    </>
   )
 }
