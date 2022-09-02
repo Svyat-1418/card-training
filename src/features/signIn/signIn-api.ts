bd3b0266b5da8ee82c27eaea37b6fc09ce821b57
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-09-02 12:32:58 +0300

    n

--- src/features/signIn/signIn-api.ts
+++ src/features/signIn/signIn-api.ts
@@ -1,6 +1,5 @@
 import { instance } from '../../common/config/apiConfig'
 
-
 export const loginAPI = {
   login({ email, password, rememberMe }: LoginParamsType) {
     return instance.post('auth/login', { email, password, rememberMe })
