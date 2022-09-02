0db8c9eb2895def615ebdac4cf4671f0c5bcd8d6
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-09-02 10:39:05 +0300

    cards page

--- src/features/cards/cardsPage.tsx
+++ src/features/cards/cardsPage.tsx
@@ -1,21 +1,37 @@
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace'
-import { CardsTable } from './Cads-table'
+import { CardsTable } from './Cards-table'
 import { useEffect } from 'react'
 import { useAppDispatch } from '../../common/hooks'
-import { getCardsThunk } from './cards-slice'
+import { addCardThunk, getCardsThunk } from './cards-slice'
+import { useSelector } from 'react-redux'
+import { RootStateType } from '../../app/store'
+import { Button } from '@mui/material'
 
 export const CardsPage = () => {
   const dispatch = useAppDispatch()
+  const queryParams = useSelector<RootStateType, any>((state) => state.cards.queryParams)
+  const packId = useSelector<RootStateType, string | undefined>(
+    (state) => state.cards.queryParams.cardsPack_id
+  )
+
+  const onAddCardHandler = () => {
+    dispatch(
+      addCardThunk({
+        card: { cardsPack_id: packId, question: 'Some question', answer: 'answer text' },
+      })
+    )
+  }
 
   useEffect(() => {
-    dispatch(getCardsThunk('заглушка'))
-  }, [])
+    dispatch(getCardsThunk({ ...queryParams }))
+  }, [packId])
   return (
     <div>
       <div>
         <span>
           <KeyboardBackspaceIcon style={{ paddingTop: '5px' }} /> Back to Pack List
         </span>
+        <Button onClick={onAddCardHandler}>add new card</Button>
       </div>
       <CardsTable />
     </div>
