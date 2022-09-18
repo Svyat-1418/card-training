99bf0fe241b87b6ae0602587ca1f3615461b110e
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-09-18 16:23:41 +0300

    rewrite thunk to async thunk

--- src/features/signUp/singnUp-slice.ts
+++ src/features/signUp/singnUp-slice.ts
@@ -1,36 +1,37 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Dispatch } from 'redux'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { handleNetworkError } from '../../common/utils/errorUtil'
 import { RegisterParamsType, signUpApi } from './signUp-api'
+import { setStatus } from '../../app/appSlice'
 
 const initialState = {
   isSignedUp: false,
 }
 
+export const registerTC = createAsyncThunk(
+  'signUp/register',
+  async (data: RegisterParamsType, { dispatch }) => {
+    dispatch(setStatus({ status: 'loading' }))
+    try {
+      const res = await signUpApi.register(data)
+      dispatch(setStatus({ status: 'idle' }))
+      return { isSignedUp: true }
+    } catch (err: any) {
+      handleNetworkError(err, dispatch)
+    }
+  }
+)
+
 const slice = createSlice({
-  name: 'login',
+  name: 'signUp',
   initialState: initialState,
-  reducers: {
-    setIsSignedUpAC(state, action: PayloadAction<{ value: boolean }>) {
-      state.isSignedUp = action.payload.value
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(registerTC.fulfilled, (state, action) => {
+      if (action.payload) {
+        state.isSignedUp = action.payload.isSignedUp
+      }
+    })
   },
 })
 
 export const registrationReducer = slice.reducer
-
-export const { setIsSignedUpAC } = slice.actions
-
-export const registerTC = (data: RegisterParamsType) => (dispatch: Dispatch) => {
-  signUpApi
-    .register(data)
-    .then((res) => {
-      debugger
-      if (res.status === 200) {
-        dispatch(setIsSignedUpAC({ value: true }))
-      }
-    })
-    .catch((error) => {
-      handleNetworkError(error, dispatch)
-    })
-}
